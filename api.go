2bdc13ade7379eeea77362853c036ee59984534e
sabmile <sab288@yandex.ru>
2023-03-06 10:40:05 +0600

    separate the data from go code

--- api.go
+++ api.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 const (
@@ -23,45 +25,38 @@ const (
 	URL_DATA           = "http://localhost:3010/data"
 )
 
-var NAMES = []string{
-	"Elisa Rush",
-	"Lulu Estes",
-	"Brogan Atkins",
-	"Alessandro Alvarez",
-	"Jerome Briggs",
-	"Maliha Petersen",
-	"Cordelia Barrett",
-	"Lexie Castillo",
-	"Harri Stevens",
-	"Veronica Ross",
-}
+var (
+	PATH_NAMES        = filepath.Join("DATA", "names.txt")
+	PATH_COMMENTS     = filepath.Join("DATA", "comments.txt")
+	PATH_DESCRIPTIONS = filepath.Join("DATA", "descriptions.txt")
 
-var COMMENTS = []string{
-	"When you take a photo, it would be good to remove your finger from the frame. In the end, it's just unprofessional. One thing is unclear: how so?!",
-	"There is simply no framing. The filter was selected unsuccessfully: I would use a series set to 80%",
-	"I lost my family, children and cat because of this photo. They said they didn't share my love for art and went to a neighbor. Everything is fine!",
-	"I'm stuck on this photo and I can't tear myself away. I don't know what to do at all. Is this a composition?! What is this composition?!",
-	"I slipped on a banana peel and dropped the camera on the cat and I got a better picture.",
-	"The faces of the people in the photo are distorted, as if they are being beaten. How could you catch such an unfortunate moment?! The horizon is littered.",
-	"My grandmother accidentally sneezed with a camera in her hands and she got a better photo. Shob I lived like this!",
-	"I can't imagine how you can photograph the sea and sunset better. This is just the apogee. After that, we can burn all the cameras, because the peak has been reached anyway. The focus is blurred. Or is it just someone splattered the lens?",
-}
+	NAMES, _        = getContent(PATH_NAMES)
+	COMMENTS, _     = getContent(PATH_COMMENTS)
+	DESCRIPTIONS, _ = getContent(PATH_DESCRIPTIONS)
 
-var DESCRIPTIONS = []string{
-	"If you clearly formulate a wish for the universe, then everything will definitely come true. Believe in yourself. The main thing is to want and dream.",
-	"Appreciate every moment. Appreciate those who are close to you and drive away all doubts. Don't offend everyone with words",
-	"How cool is the food here",
-	"Hanging out with friends at the sea",
-}
+	generateCommentId          = createIdGenerator()
+	generatePictureId          = createIdGenerator()
+	data              pictures = getPictures(COUNT_PICTURES, newPicture)
+)
 
-type comment struct {
-	ID      uint8  `json:"id"`
-	Avatar  string `json:"avatar"`
-	Message string `json:"message"`
-	Name    string `json:"name"`
-}
+type (
+	comment struct {
+		ID      uint8  `json:"id"`
+		Avatar  string `json:"avatar"`
+		Message string `json:"message"`
+		Name    string `json:"name"`
+	}
 
-var generateCommentId = createIdGenerator()
+	picture struct {
+		ID          uint8      `json:"id"`
+		URL         string     `json:"url"`
+		Likes       uint16     `json:"likes"`
+		Comments    []*comment `json:"comments"`
+		Description string     `json:"description"`
+	}
+
+	pictures []*picture
+)
 
 func newComment() *comment {
 	var id = generateCommentId()
@@ -73,16 +68,6 @@ func newComment() *comment {
 	}
 }
 
-type picture struct {
-	ID          uint8      `json:"id"`
-	URL         string     `json:"url"`
-	Likes       uint16     `json:"likes"`
-	Comments    []*comment `json:"comments"`
-	Description string     `json:"description"`
-}
-
-var generatePictureId = createIdGenerator()
-
 func newPicture() *picture {
 	var id = generatePictureId()
 	return &picture{
@@ -93,10 +78,6 @@ func newPicture() *picture {
 		Description: getRandom(DESCRIPTIONS),
 	}
 }
-
-type pictures []*picture
-
-var data pictures = getPictures(COUNT_PICTURES, newPicture)
 
 func createIdGenerator() func() uint8 {
 	var lastGeneratedId uint8
@@ -153,6 +134,23 @@ func loadData(file string) {
 func processForm(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(1024 * 10)
 	fmt.Println(w, r.MultipartForm)
+}
+
+func getContent(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, scanner.Err()
 }
 
 func getData(w http.ResponseWriter, r *http.Request) {
